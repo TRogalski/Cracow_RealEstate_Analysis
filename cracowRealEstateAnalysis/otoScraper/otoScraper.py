250f8598ca6feb17d66de2b762e386b780909a23
TRogalski <tomracc@gmail.com>
2020-03-01 23:35:14 +0100

    Code reformat

--- cracowRealEstateAnalysis/otoScraper/otoScraper.py
+++ cracowRealEstateAnalysis/otoScraper/otoScraper.py
@@ -7,48 +7,48 @@ class OtoScraper:
     page_url_template="https://www.otodom.pl/sprzedaz/mieszkanie/krakow/?search%5Bfilter_float_m%3Afrom%5D=25&search%5Bregion_id%5D=6&search%5Bcity_id%5D=38&search%5Bpaidads_listing%5D=1&nrAdsPerPage=72&page={}"
     
     def get_all_offer_links(self, n):
-        pageId = 1
-        offerLinks = []
+        page_id = 1
+        offer_links = []
         
-        response = requests.get(self.page_url_template.format(pageId))
+        response = requests.get(self.page_url_template.format(page_id))
         
-        while response.status_code == 200 and pageId<n:
-            offerLinks += self.get_page_offer_links(response)
-            pageId+=1
-            response = requests.get(self.page_url_template.format(pageId))
-        return offerLinks
+        while response.status_code == 200 and page_id<n:
+            offer_links += self.get_page_offer_links(response)
+            page_id+=1
+            response = requests.get(self.page_url_template.format(page_id))
+        return offer_links
     
     
-    def get_page_offer_links(self, httpResponse):
-        html_soup = BeautifulSoup(httpResponse.content, 'html.parser')
-        offerSections = html_soup.find_all(attrs={"data-url" : re.compile(r".*")})
-        links = self.get_links_from_articles(offerSections)
+    def get_page_offer_links(self, http_response):
+        html_soup = BeautifulSoup(http_response.content, 'html.parser')
+        offer_sections = html_soup.find_all(attrs={"data-url" : re.compile(r".*")})
+        links = self.get_links_from_articles(offer_sections)
         return links 
         
     
-    def get_links_from_articles(self, offerSections):
+    def get_links_from_articles(self, offer_sections):
         links = []
-        for offer in offerSections:
+        for offer in offer_sections:
             links.append(offer['data-url'])
         return links
     
     
-    def get_data_from_offer_links(self, offerLinks):
-        offerData = {}
-        for link in offerLinks:
+    def get_data_from_offer_links(self, offer_links):
+        offer_data = {}
+        for link in offer_links:
             response = requests.get(link)
-            htmlSoup = BeautifulSoup(response.content, 'html.parser')
-            offerDetailsSection = htmlSoup.find(class_ = "section-overview")
-            detailsList = offerDetailsSection.find_all('li')
-            offerData[link] = self.format_offer_details(detailsList)
-            print(offerData)
+            html_soup = BeautifulSoup(response.content, 'html.parser')
+            offer_details_section = html_soup.find(class_ = "section-overview")
+            details_list = offer_details_section.find_all('li')
+            offer_data[link] = self.format_offer_details(details_list)
+            print(offer_data)
     
-    def format_offer_details(self, detailsList):
-        formattedDetails = {}
-        for detail in detailsList:
+    def format_offer_details(self, details_list):
+        formatted_details = {}
+        for detail in details_list:
             k, v = detail.text.split(':')
-            formattedDetails[k] = v
-        return formattedDetails
+            formatted_details[k] = v
+        return formatted_details
     
 if __name__ == "__main__":
     otoScraper = OtoScraper()
