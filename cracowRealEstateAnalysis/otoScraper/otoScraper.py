ed68b18dcaeabb88db6f73be2bdb356f3bbd22c1
TRogalski <tomracc@gmail.com>
2020-03-02 00:17:46 +0100

    Added response error logs

--- cracowRealEstateAnalysis/otoScraper/otoScraper.py
+++ cracowRealEstateAnalysis/otoScraper/otoScraper.py
@@ -11,12 +11,18 @@ class OtoScraper:
         offer_links = []
         
         response = requests.get(self.page_url_template.format(page_id))
-        
-        while response.status_code == 200 and page_id<n:
-            offer_links += self.get_page_offer_links(response)
+        errors = {}
+      
+        while page_id<=n:
+            if response.status_code != 200:
+                errors[page_id] = response.status_code
+                print("Could not retrieve from {}".format(page_id))
+            else:
+                print("Gathering links from page no. {}".format(page_id))
+                offer_links += self.get_page_offer_links(response)
             page_id+=1
             response = requests.get(self.page_url_template.format(page_id))
-        return offer_links
+        return offer_links, errors
     
     
     def get_page_offer_links(self, http_response):
@@ -35,13 +41,19 @@ class OtoScraper:
     
     def get_data_from_offer_links(self, offer_links):
         offer_data = {}
+        errors = {}
         for link in offer_links:
             response = requests.get(link)
-            html_soup = BeautifulSoup(response.content, 'html.parser')
-            offer_details_section = html_soup.find(class_ = "section-overview")
-            details_list = offer_details_section.find_all('li')
-            offer_data[link] = self.format_offer_details(details_list)
-            print(offer_data)
+            if response.status_code != 200:
+                errors[link] = response.status_code
+                print("Could not retrieve from {}".format(link))
+            else:
+                html_soup = BeautifulSoup(response.content, 'html.parser')
+                offer_details_section = html_soup.find(class_ = "section-overview")
+                details_list = offer_details_section.find_all('li')
+                offer_data[link] = self.format_offer_details(details_list)
+                print("Gathering links from page {}".format(link))
+        return offer_data, errors
     
     def format_offer_details(self, details_list):
         formatted_details = {}
@@ -52,5 +64,8 @@ class OtoScraper:
     
 if __name__ == "__main__":
     otoScraper = OtoScraper()
-    links = otoScraper.get_all_offer_links(4)
-    otoScraper.get_data_from_offer_links(links)
+    links,  errors = otoScraper.get_all_offer_links(360)
+    data, offer_errors = otoScraper.get_data_from_offer_links(links)
+    print(errors)
+    print("---------------------------")
+    print(offer_errors)
